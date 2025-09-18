f50ad9eb811e8f4e4334c6e8638d8b1fcacf09bc
parknahye-dot <a207903@gmail.com>
2025-09-18 22:00:48 +0900

    7

--- aa.java
+++ aa.java
@@ -1,8 +1,7 @@
 public class aa {
     public static void main(String[] args) {
-        char charValue = 'A';
-        int intValue = charValue;
-        System.out.println("Character: " + charValue);
-        System.out.println("Integer value: " + intValue);
+        char a= 'a';
+        char b=(a+1);
+        System.out.println(b);
     }
 }
